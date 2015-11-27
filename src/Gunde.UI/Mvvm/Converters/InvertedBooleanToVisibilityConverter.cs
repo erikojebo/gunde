1b8ce2800a872b541a50712023bbc7eb3d727f56
Erik Öjebo <erik.ojebo@gmail.com>
2015-11-27 21:03:59 +0100

    Added inverted bool2visibility converter

---
+++ src/Gunde.UI/Mvvm/Converters/InvertedBooleanToVisibilityConverter.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace Gunde.UI.Mvvm.Converters
+{
+    public class InvertedBooleanToVisibilityConverter : BooleanConverter<Visibility>
+    {
+        public InvertedBooleanToVisibilityConverter() : base(Visibility.Collapsed, Visibility.Hidden) { }
+    }
+}
